8d1c22310b5493f216e6fac021a7d99ce09e7494
vedricopane <vedricopane04@gmail.com>
2022-11-24 22:12:48 +0700

    add custom tab buttom navigator

--- src/router/index.js
+++ src/router/index.js
@@ -11,23 +11,24 @@ import {
   Hospitals,
 } from '../pages';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import { BottomNavigator } from '../components';
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
 const MainApp = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator tabBar={props => <BottomNavigator {...props}/>}>
       <Tab.Screen name="Doctor" component={Doctor} options={{ headerShown: false }} />
       <Tab.Screen name="Messages" component={Messages} options={{headerShown: false}}/>
-      <Tab.Screen name="Hospital" component={Hospitals} options={{ headerShown: false }} />
+      <Tab.Screen name="Hospitals" component={Hospitals} options={{ headerShown: false }} />
     </Tab.Navigator>
   );
 };
 
 const Router = () => {
   return (
-    <Stack.Navigator initialRouteName="Splash">
+    <Stack.Navigator initialRouteName="MainApp">
       <Stack.Screen
         name="Splash"
         component={Splash}
