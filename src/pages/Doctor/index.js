57d155ee182b437136293bf62c34ed46e0f125c8
vedricopane <vedricopane04@gmail.com>
2022-11-25 15:17:30 +0700

    add horizontal scroll category doctor & section label in Doctor page

--- src/pages/Doctor/index.js
+++ src/pages/Doctor/index.js
@@ -1,7 +1,8 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {ScrollView, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import {
   DoctorCategory,
+  Gap,
   HomeProfile,
   NewsItem,
   RatedDoctor,
@@ -11,22 +12,32 @@ import {colors, fonts} from '../../utils';
 const Doctor = () => {
   return (
     <View style={styles.page}>
-      <HomeProfile />
-      <Text style={styles.welcomeText}>
-        Mau konsultasi dengan siapa hari ini?
-      </Text>
-      <DoctorCategory />
-      <DoctorCategory />
-      <DoctorCategory />
-      <DoctorCategory />
-      <Text>Top Rated Doctors</Text>
-      <RatedDoctor />
-      <RatedDoctor />
-      <RatedDoctor />
-      <Text>Good News</Text>
-      <NewsItem />
-      <NewsItem />
-      <NewsItem />
+      <View style={styles.content}>
+        <HomeProfile />
+        <Text style={styles.welcomeText}>
+          Mau konsultasi dengan siapa hari ini?
+        </Text>
+        <View style={styles.wrapperScroll}>
+          <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+            <View style={styles.category}>
+              <Gap width={16} />
+              <DoctorCategory />
+              <DoctorCategory />
+              <DoctorCategory />
+              <DoctorCategory />
+              <Gap width={6} />
+            </View>
+          </ScrollView>
+        </View>
+        <Text style={styles.sectionLabel}>Top Rated Doctors</Text>
+        <RatedDoctor />
+        <RatedDoctor />
+        <RatedDoctor />
+        <Text style={styles.sectionLabel}>Good News</Text>
+        <NewsItem />
+        <NewsItem />
+        <NewsItem />
+      </View>
     </View>
   );
 };
@@ -35,16 +46,36 @@ export default Doctor;
 
 const styles = StyleSheet.create({
   page: {
+    backgroundColor: colors.secondary,
+    flex: 1,
+  },
+  content: {
+    backgroundColor: colors.white,
+    flex: 1,
     paddingHorizontal: 16,
     paddingVertical: 30,
-    flex: 1,
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
   },
   welcomeText: {
     fontSize: 20,
     fontFamily: fonts.primary[600],
     color: colors.text.primary,
     marginTop: 30,
-    marginBottom: 16, 
-    maxWidth: 209
+    marginBottom: 16,
+    maxWidth: 209,
+  },
+  wrapperScroll: {
+    marginHorizontal: -16, //agar tidak ada padding, maka angka 16 diambil dari padding horizontal pada styles.page
+  },
+  category: {
+    flexDirection: 'row',
+  },
+  sectionLabel: {
+    fontSize: 16,
+    fontFamily: fonts.primary[600],
+    color: colors.text.primary,
+    marginTop: 30,
+    marginBottom: 16,
   },
 });
