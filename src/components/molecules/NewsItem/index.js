2b10cfe22e291c1bf5ee615a06e1923fc589d80d
vedricopane <vedricopane04@gmail.com>
2022-11-25 12:06:33 +0700

    add base component Doctor page

---
+++ src/components/molecules/NewsItem/index.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const NewsItem = () => {
+  return (
+    <View>
+      <Text>NewsItem Component</Text>
+    </View>
+  )
+}
+
+export default NewsItem
+
+const styles = StyleSheet.create({})
--- src/pages/Doctor/index.js
+++ src/pages/Doctor/index.js
@@ -1,14 +1,28 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {DoctorCategory, HomeProfile, NewsItem, RatedDoctor} from '../../components';
 
 const Doctor = () => {
   return (
     <View>
-      <Text>Doctor Page</Text>
+      <HomeProfile />
+      <Text>Mau konsultasi dengan siapa hari ini?</Text>
+      <DoctorCategory />
+      <DoctorCategory />
+      <DoctorCategory />
+      <DoctorCategory />
+      <Text>Top Rated Doctors</Text>
+      <RatedDoctor />
+      <RatedDoctor />
+      <RatedDoctor />
+      <Text>Good News</Text>
+      <NewsItem />
+      <NewsItem />
+      <NewsItem />
     </View>
-  )
-}
+  );
+};
 
-export default Doctor
+export default Doctor;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
